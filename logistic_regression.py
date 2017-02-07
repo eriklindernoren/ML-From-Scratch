3840a07506e44c34c842a03209856d35c5b72702
eriklindernoren <eriklindernoren@live.se>
2017-02-07 16:28:49 +0100

    Cleanup

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 import math
 
@@ -15,33 +14,34 @@ def make_diagonal(x):
 		m[i,i] = x[i]
 	return m
 
-# test predictions
-dataset = np.array([[1, 2.7810836,2.550537003,0],
-	[1, 1.465489372,2.362125076,0],
-	[1, 3.396561688,4.400293529,0],
-	[1, 1.38807019,1.850220317,0],
-	[1, 3.06407232,3.005305973,0],
-	[1, 7.627531214,2.759262235,1],
-	[1, 5.332441248,2.088626775,1],
-	[1, 6.922596716,1.77106367,1],
-	[1, 8.675418651,-0.242068655,1],
-	[1, 7.673756466,3.508563011,1]])
+# Data
+dataset = np.array([	[1,2.7810836,2.550537003,		0],
+						[1,1.465489372,2.362125076,		0],
+						[1,3.396561688,4.400293529,		0],
+						[1,1.38807019,1.850220317,		0],
+						[1,3.06407232,3.005305973,		0],
+						[1,7.627531214,2.759262235,		1],
+						[1,5.332441248,2.088626775,		1],
+						[1,6.922596716,1.77106367,		1],
+						[1,8.675418651,-0.242068655,	1],
+						[1,7.673756466,3.508563011,		1]
+						])
 
 x_train = dataset[:,0:-1]
 y_train = dataset[:,-1].reshape((len(dataset[:,-1]),1))
 
 n_features = len(x_train[0])
 n_iterations = 5
-learning_rate = 0.001
 
 # Initial weights between [-1/sqrt(N), 1/sqrt(N)] (w - hidden, v - output)
 a = -1/math.sqrt(n_features)
 b = -a
-parameters = (b-a)*np.random.random((len(x_train[0]), 1)) + a
+param = (b-a)*np.random.random((len(x_train[0]), 1)) + a
 
+# Tune parameters for n iterations
 for i in range(n_iterations):
 	# Make a new prediction
-	dot = x_train.dot(parameters)
+	dot = x_train.dot(param)
 	y_pred = sigmoid(dot)
 
 	# Make a diagonal matrix of the sigmoid gradient column vector
@@ -49,8 +49,9 @@ for i in range(n_iterations):
 
 	# Batch opt:
 	# (X^T * diag(sigm*(1 - sigm) * X) * X^T * (diag(sigm*(1 - sigm) * X * param + Y - Y_pred)
-	parameters = np.linalg.inv(x_train.T.dot(diag_gradient).dot(x_train)).dot(x_train.T).dot(diag_gradient.dot(x_train).dot(parameters) + y_train - y_pred)
+	param = np.linalg.inv(x_train.T.dot(diag_gradient).dot(x_train)).dot(x_train.T).dot(diag_gradient.dot(x_train).dot(param) + y_train - y_pred)
 
+# Print the last prediction
 print "Y prediction:"
 print y_pred
 print "Y:"
