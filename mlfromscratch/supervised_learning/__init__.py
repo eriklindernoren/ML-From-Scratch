4593ffbb64399ed9eaf4921d6c0d5430d81ec1cb
eriklindernoren <eriklindernoren@live.se>
2017-09-14 01:28:46 +0200

    Resolves #22

--- mlfromscratch/supervised_learning/__init__.py
+++ mlfromscratch/supervised_learning/__init__.py
@@ -4,13 +4,13 @@ from .decision_tree import RegressionTree, ClassificationTree, XGBoostRegression
 from .gradient_boosting import GradientBoostingClassifier, GradientBoostingRegressor
 from .k_nearest_neighbors import KNN
 from .linear_discriminant_analysis import LDA
-from .linear_regression import LinearRegression, PolynomialRegression
+from .regression import LinearRegression, PolynomialRegression
+from .regression import RidgeRegression, PolynomialRidgeRegression
 from .logistic_regression import LogisticRegression
 from .multi_class_lda import MultiClassLDA
 from .neural_network import NeuralNetwork
 from .naive_bayes import NaiveBayes
 from .perceptron import Perceptron
 from .random_forest import RandomForest
-from .ridge_regression import RidgeRegression
 from .support_vector_machine import SupportVectorMachine
 from .xgboost import XGBoost
