af496778f47ade9a3629ec1c8fa0595117af358a
David Lee <dwlee@pku.edu.cn>
2018-11-02 15:45:13 +0800

    Clean up import
    standardize and calculate_correlation_matrix haven't been used

--- mlfromscratch/unsupervised_learning/principal_component_analysis.py
+++ mlfromscratch/unsupervised_learning/principal_component_analysis.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, division
 import numpy as np
-from mlfromscratch.utils import calculate_covariance_matrix, standardize
-from mlfromscratch.utils import calculate_correlation_matrix
+from mlfromscratch.utils import calculate_covariance_matrix
 
 
 class PCA():
