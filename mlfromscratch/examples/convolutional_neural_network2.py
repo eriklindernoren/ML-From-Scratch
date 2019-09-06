ad8ad5a7b6546f88d5d4bbaa9d03b4d154faf3da
aidevnn <47622664+aidevnn@users.noreply.github.com>
2019-09-06 04:01:43 +0300

    CNN with MaxPool2D example

---
+++ mlfromscratch/examples/convolutional_neural_network2.py
@@ -0,0 +1,87 @@
+
+from __future__ import print_function
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+# Import helper functions
+from mlfromscratch.deep_learning import NeuralNetwork
+from mlfromscratch.utils import train_test_split, to_categorical, normalize
+from mlfromscratch.utils import get_random_subsets, shuffle_data, Plot
+from mlfromscratch.utils.data_operation import accuracy_score
+from mlfromscratch.deep_learning.optimizers import StochasticGradientDescent, Adam, RMSprop, Adagrad, Adadelta
+from mlfromscratch.deep_learning.loss_functions import CrossEntropy
+from mlfromscratch.utils.misc import bar_widgets
+from mlfromscratch.deep_learning.layers import Dense, Dropout, Conv2D, Flatten, Activation, MaxPooling2D
+from mlfromscratch.deep_learning.layers import AveragePooling2D, ZeroPadding2D, BatchNormalization, RNN
+
+
+
+def main():
+
+    #----------
+    # Conv Net
+    #----------
+
+    optimizer = Adagrad()
+
+    data = datasets.load_digits()
+    X = data.data
+    y = data.target
+    
+    # Convert to one-hot encoding
+    y = to_categorical(y.astype("int"))
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, seed=1)
+
+    # Reshape X to (n_samples, channels, height, width)
+    X_train = X_train.reshape((-1,1,8,8))
+    X_test = X_test.reshape((-1,1,8,8))
+
+    clf = NeuralNetwork(optimizer=optimizer,
+                        loss=CrossEntropy,
+                        validation_data=(X_test, y_test))
+
+    clf.add(Conv2D(n_filters=16, filter_shape=(3,3), stride=1, input_shape=(1,8,8), padding='same'))
+    clf.add(Activation('relu'))
+    clf.add(Conv2D(n_filters=32, filter_shape=(3,3), stride=1, padding='same'))
+    clf.add(Activation('relu'))
+    clf.add(MaxPooling2D(pool_shape=(2, 2), stride=2, padding='valid'))
+    clf.add(Dropout(0.25))
+    clf.add(BatchNormalization())
+    clf.add(Flatten())
+    clf.add(Dense(256))
+    clf.add(Activation('relu'))
+    clf.add(Dropout(0.4))
+    clf.add(Dense(32))
+    clf.add(Activation('relu'))
+    clf.add(Dense(10))
+    clf.add(Activation('softmax'))
+
+    print ()
+    clf.summary(name="ConvNet")
+
+    train_err, val_err = clf.fit(X_train, y_train, n_epochs=50, batch_size=64)
+
+    # Training and validation error plot
+    n = len(train_err)
+    training, = plt.plot(range(n), train_err, label="Training Error")
+    validation, = plt.plot(range(n), val_err, label="Validation Error")
+    plt.legend(handles=[training, validation])
+    plt.title("Error Plot")
+    plt.ylabel('Error')
+    plt.xlabel('Iterations')
+    plt.show()
+
+    _, accuracy = clf.test_on_batch(X_test, y_test)
+    print ("Accuracy:", accuracy)
+
+
+    y_pred = np.argmax(clf.predict(X_test), axis=1)
+    X_test = X_test.reshape(-1, 8*8)
+    # Reduce dimension to 2D using PCA and plot the results
+    Plot().plot_in_2d(X_test, y_pred, title="Convolutional Neural Network", accuracy=accuracy, legend_labels=range(10))
+
+if __name__ == "__main__":
+    main()
